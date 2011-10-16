910ed1e7efc63228654650825520f18b5fbebfbd
antirez <antirez@gmail.com>
2011-10-16 10:41:41 +0200

    news submission work in progress

--- app.rb
+++ app.rb
@@ -1,3 +1,30 @@
+# Copyright 2011 Salvatore Sanfilippo. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+#    1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 
+#    2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY SALVATORE SANFILIPPO ''AS IS'' AND ANY EXPRESS
+# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+# NO EVENT SHALL SALVATORE SANFILIPPO OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# 
+# The views and conclusions contained in the software and documentation are
+# those of the authors and should not be interpreted as representing official
+# policies, either expressed or implied, of Salvaore Sanfilippo.
+
 require 'rubygems'
 require 'redis'
 require 'page'
@@ -75,6 +102,54 @@ get '/api/create_account' do
     end
 end
 
+get '/api/submit' do
+    return {:status => "err", :error => "Not authenticated."}.to_json if !$user
+    # We can have an empty url or an empty first comment, but not both.
+    if (!check_params "title",:url,:text) or (params[:url].length == 0 and
+                                              params[:text].length == 0)
+        return {
+            :status => "err",
+            :error => "Please specify a news title and address or text."
+        }.to_json
+    end
+    # Make sure the URL is about an acceptable protocol, that is
+    # http:// or https:// for now.
+    if params[:url].length != 0
+        if params[:url].index("http://") != 0 and
+           params[:url].index("https://") != 0
+            return {
+                :status => "err",
+                :error => "We only accept http:// and https:// news."
+            }.to_json
+        end
+    end
+    news_id = submit_news(params[:title],params[:url],params[:text],$user["id"])
+    return  {
+        :status => "ok",
+        :news_id => news_id
+    }.to_json
+end
+
+# Check that the list of parameters specified exist.
+# If at least one is missing false is returned, otherwise true is returned.
+#
+# If a parameter is specified as as symbol only existence is tested.
+# If it is specified as a string the parameter must also meet the condition
+# of being a non empty string.
+def check_params *required
+    required.each{|p|
+        if !params[p] or (p.is_a? String and params[p].length == 0)
+            return false
+        end
+    }
+    true
+end
+
+def check_params_or_halt *required
+    return if check_parameters *required
+    halt 500, H.h1{"500"}+H.p{"Missing parameters"}
+end
+
 def application_header
     navitems = [    ["top","/"],
                     ["latest","/latest"],
@@ -109,6 +184,10 @@ def application_footer
         H.a(:href=>"http://github.com/antirez/lamernews"){"here"}
     }
 end
+
+################################################################################
+# User and authentication
+################################################################################
 
 # Try to authenticate the user, if the credentials are ok we populate the
 # $user global with the user information.
@@ -195,4 +274,41 @@ def check_user_credentials(username,password)
     user = get_user_by_username(username)
     return nil if !user
     (user['password'] == hp) ? user['auth'] : nil
+end
+
+################################################################################
+# News
+################################################################################
+
+# Add a news with the specified url or text.
+#
+# If an url is passed but was already posted in the latest 48 hours the
+# news is not inserted, and the ID of the old news with the same URL is
+# returned.
+#
+# Return value: the ID of the inserted news, or the ID of the news with
+# the same URL recently added.
+def submit_news(title,url,text,user_id)
+    # If we don't have an url but a comment, we turn the url into
+    # text://....first comment..., so it is just a special case of
+    # title+url anyway.
+    textpost = url.length == 0
+    if url.length == 0
+        url = "text://"+text[0...CommentMaxLength]
+    end
+    # Check for already posted news with the same URL.
+    if !textpost and (id = $r.get("url+"+url))
+        return id
+    end
+    # We can finally insert the news.
+    ctime = Time.new.to_i
+    id = $r.incr("news.count")
+    $r.hmset("news:#{id}",
+        "id", id,
+        "title", title,
+        "url", url,
+        "user_id", user_id,
+        "ctime", ctime,
+        "score", 0,
+        "rank", 0)
 end
--- app_config.rb
+++ app_config.rb
@@ -2,3 +2,4 @@ SiteName = "Lamer News"
 RedisHost = "127.0.0.1"
 RedisPort = 6379
 PasswordSalt = "*LAMER*news*"
+CommentMaxLength = 4096

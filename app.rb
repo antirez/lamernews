1684fe044cb0a6631b65f8d4448843938c5f4422
Glenn Oppegard <oppegard@gmail.com>
2011-10-20 16:05:11 -0700

    Added support for deploying to Cloud Foundry via 'bundle package' and 'vmc push'

--- app.rb
+++ app.rb
@@ -2,14 +2,14 @@
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 #    1. Redistributions of source code must retain the above copyright
 #    notice, this list of conditions and the following disclaimer.
-# 
+#
 #    2. Redistributions in binary form must reproduce the above copyright
 #    notice, this list of conditions and the following disclaimer in the
 #    documentation and/or other materials provided with the distribution.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY SALVATORE SANFILIPPO ''AS IS'' AND ANY EXPRESS
 # OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
 # OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
@@ -20,7 +20,7 @@
 # ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 # THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-# 
+#
 # The views and conclusions contained in the software and documentation are
 # those of the authors and should not be interpreted as representing official
 # policies, either expressed or implied, of Salvaore Sanfilippo.
@@ -36,8 +36,19 @@ require 'digest/sha1'
 require 'digest/md5'
 require 'comments'
 
+$vcap_services = JSON.parse(ENV['VCAP_SERVICES']) if ENV['VCAP_SERVICES']
+
 before do
-    $r = Redis.new(:host => RedisHost, :port => RedisPort) if !$r
+    redis_conf = {}
+    if $vcap_services
+      redis = $vcap_services['redis-2.2'][0]
+      redis_conf = {:host => redis['credentials']['hostname'],
+                    :port => redis['credentials']['port'],
+                    :password => redis['credentials']['password']}
+    else
+      redis_conf = {:host => RedisHost, :port => RedisPort}
+    end
+    $r = Redis.new(redis_conf) if !$r
     H = HTMLGen.new if !defined?(H)
     if !defined?(Comments)
         Comments = RedisComments.new($r,"comment",proc{|c,level|
@@ -343,7 +354,7 @@ end
 get '/api/login' do
     auth,apisecret = check_user_credentials(params[:username],
                                             params[:password])
-    if auth 
+    if auth
         return {
             :status => "ok",
             :auth => auth,
@@ -359,7 +370,7 @@ end
 
 post '/api/create_account' do
     auth = create_user(params[:username],params[:password])
-    if auth 
+    if auth
         return {:status => "ok", :auth => auth}.to_json
     else
         return {
@@ -432,7 +443,7 @@ post '/api/votenews' do
     if vote_news(params["news_id"].to_i,$user["id"],vote_type)
         return { :status => "ok" }.to_json
     else
-        return { :status => "err", 
+        return { :status => "err",
                  :error => "Invalid parameters or duplicated vote." }.to_json
     end
 end
@@ -515,7 +526,7 @@ def application_header
         if $user
             text = $user['username']
             link = "/user/"+H.urlencode($user['username'])
-            H.a(:href => "/user/"+H.urlencode($user['username'])) { 
+            H.a(:href => "/user/"+H.urlencode($user['username'])) {
                 $user['username']+" (#{$user['karma']})"
             }+" | "+
             H.a(:href =>
@@ -731,7 +742,7 @@ end
 
 # Vote the specified news in the context of a given user.
 # type is either :up or :down
-# 
+#
 # The function takes care of the following:
 # 1) The vote is not duplicated.
 # 2) That the karma is decreased from voting user, accordingly to vote type.
@@ -976,7 +987,7 @@ end
 
 # Generate the main page of the web site, the one where news are ordered by
 # rank.
-# 
+#
 # As a side effect thsi function take care of checking if the rank stored
 # in the DB is no longer correct (as time is passing) and updates it if
 # needed.

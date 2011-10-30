f59e24fcf60a42566afb8f85025c037d05c06235
antirez <antirez@gmail.com>
2011-10-30 14:21:35 +0100

    version 0.9.0

--- app.rb
+++ app.rb
@@ -38,7 +38,7 @@ require 'comments'
 require 'pbkdf2'
 require 'openssl' if UseOpenSSL
 
-Version = "0.8.0"
+Version = "0.9.0"
 
 before do
     $r = Redis.new(:host => RedisHost, :port => RedisPort) if !$r

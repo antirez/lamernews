fcf62e7a1ae9e28145101e65249adefc22a97500
Frederic Cambus <fcambus@users.sourceforge.net>
2013-06-20 13:01:39 +0200

    Modifying the regexp in url_to_links function in order to detect URLs containing colons

--- app.rb
+++ app.rb
@@ -1872,7 +1872,7 @@ end
 # HTML links. We try to handle the case of an url that is followed by a period
 # Like in "I suggest http://google.com." excluding the final dot from the link.
 def urls_to_links(s)
-    urls = /((https?:\/\/|www\.)([-\w\.]+)+(:\d+)?(\/([\w\/_#\.\-\%]*(\?\S+)?)?)?)/
+    urls = /((https?:\/\/|www\.)([-\w\.]+)+(:\d+)?(\/([\w\/_#:\.\-\%]*(\?\S+)?)?)?)/
     s.gsub(urls) {
         url = text = $1
         url = "http://#{url}" if $2 == 'www.'
